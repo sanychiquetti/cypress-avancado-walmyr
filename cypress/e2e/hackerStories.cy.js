35c2f795bf1fb99e6902e019a23622629f1ed27d
sanychiquetti <sanymara77@hotmail.com>
2023-10-01 13:53:07 -0300

    exemplo de buscar um elemento com seletor muito grande

--- cypress/e2e/hackerStories.cy.js
+++ cypress/e2e/hackerStories.cy.js
@@ -256,7 +256,7 @@ describe('Hacker Stories', () => {
           .should('be.visible')
       })
       context('Last searches', () => {
-        it('shows a max of 5 buttons for the last searched terms', () => {
+        it.only('shows a max of 5 buttons for the last searched terms', () => {
           const faker = require('faker')
 
           cy.intercept(
@@ -271,8 +271,11 @@ describe('Hacker Stories', () => {
             cy.wait('@getRandomStories')
           })
 
-          cy.get('.last-searches button')
-            .should('have.length', 5)
+          cy.get('.last-searches')
+            .within(() => {
+              cy.get('button')
+                .should('have.length', 5)
+            })
         })
       })
     })
