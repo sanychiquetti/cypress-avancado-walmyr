39ee44541a6216aab5e15582b00c02523ee82dbb
sanychiquetti <sanymara77@hotmail.com>
2023-10-01 11:17:06 -0300

    finalizando testes que estavam pendentes

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,9 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
+    setupNodeEvents (on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'https://wlsf82-hacker-stories.web.app',
-  },
+    baseUrl: 'https://wlsf82-hacker-stories.web.app'
+  }
 })
